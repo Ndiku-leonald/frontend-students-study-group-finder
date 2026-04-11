652a7d07b0dac64586886015836c6863c78da3a8
Elimu Brian Joshua <bryanjelimu@gmail.com>
2026-04-11 13:58:22 +0300

    Update App.js
    Add routes for group creation, chat, and session

--- components/App.js
+++ components/App.js
@@ -3,7 +3,13 @@ import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import CreateGroup from './pages/CreateGroup';
+import GroupChat from './pages/GroupChat';
+import CreateSession from './pages/CreateSession';
 
+<Route path="/create-group" element={<CreateGroup />} />
+<Route path="/chat" element={<GroupChat />} />
+<Route path="/session" element={<CreateSession />} />
 <Routes>
   <Route path="/" element={<Dashboard />} />
   <Route path="/login" element={<Login />} />
